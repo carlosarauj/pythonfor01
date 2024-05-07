9cd6cf1b23722c51e0beba4f33647c29fb1993a7
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-05-07 19:56:01 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,12 @@
+nomes = ["Carlos", "Beto", "Alfredo"]
+
+# for index in range(len(nomes)):
+#     print(nomes[index],index)
+
+for index in range(5):
+    if index == 0:
+        print("primeira linha")
+    else:
+        print(f"linha {index}")
+
+#usa o terminal com: python nomedoarquivo.py
